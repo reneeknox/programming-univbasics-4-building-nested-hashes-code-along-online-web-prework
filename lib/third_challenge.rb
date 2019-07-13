59daaeddebf096965d2962948fc5738ca0c86a39
Renee France <renee5.0@outlook.com>
2019-07-13 03:31:17 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,14 +1,13 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-        #your key/value pairs here
+      :patriarch => {name: "Lord Montague", age: "53"},
         },
       :matriarch => {
-        #your key/value pairs here
+        {name: "Lady Montague", age: "54"}
         },
-      :hero => {
-        #your key/value pairs here
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+        h
         },
       :hero_friends => []
    }, 
